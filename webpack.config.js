09cbbf72056c5a8c7edbbfefd46751305132240d
Philip <belousovfil@mail.ru>
2020-06-08 15:03:45 +0700

    Finish css layout

--- webpack.config.js
+++ webpack.config.js
@@ -9,85 +9,92 @@ const isDev = !isProd
 
 const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 const jsLoaders = () => {
-  const loaders = {
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env'],
-    },
-  }
-  if (isDev) {
-    loaders.push('eslint-loader')
-  }
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        },
+    ]
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
 
-  return loaders
+    return loaders
 }
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: ['@babel/polyfill', './app/index.js'],
-  output: {
-    filename: filename('js'),
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill', './app/index.js'],
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
     },
-  },
-  devServer: {
-    port: 4200,
-    hot: isDev,
-  },
-  devtool: isDev ? 'source-map' : false,
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      favicon: 'favicon.ico',
-    }),
-    new CleanWebpackPlugin,
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
         },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: filename('css'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.sass$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadall: true,
-            },
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: jsLoaders,
-        loader: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+    },
+    devServer: {
+        port: 4200,
+        hot: isDev,
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            favicon: 'favicon.ico',
+        }),
+        new CleanWebpackPlugin,
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css'),
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.sass$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadall: true,
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders(),
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 }
 
